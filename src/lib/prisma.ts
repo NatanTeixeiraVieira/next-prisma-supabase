a989b5b091e2b6cb6db8c2b25705812d36d94340
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2025-05-17 00:35:30 -0300

    ci: Config production build with prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,4 @@
+import { PrismaClient } from "../../prisma/generated/prisma"
 
-import { PrismaClient } from '../generated/prisma'
-import { withAccelerate } from '@prisma/extension-accelerate'
-
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
-}
-
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
+const prisma = new PrismaClient()
 export default prisma
